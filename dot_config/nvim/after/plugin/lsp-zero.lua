a48f739e9c14551e13d4236ee8b88fab8a039a74
Sax Dhiut <mail@iyxeyl.me>
2023-06-27 01:59:58 +0400

    For real, this time

--- dot_config/nvim/after/plugin/lsp-zero.lua
+++ dot_config/nvim/after/plugin/lsp-zero.lua
@@ -1,7 +1,16 @@
 local lsp = require("lsp-zero").preset({})
 
 lsp.on_attach(function(_, bufnr)
-	lsp.default_keymaps({ buffer = bufnr })
+	local opts = { buffer = bufnr }
+	lsp.default_keymaps(opts)
+
+	vim.keymap.set("n", "<leader>gr", "<cmd>Telescope lsp_references<cr>", opts)
+	vim.keymap.set("n", "<leader>gi", "<cmd>Telescope lsp_incoming_calls<cr>", opts)
+	vim.keymap.set("n", "<leader>go", "<cmd>Telescope lsp_outgoing_calls<cr>", opts)
+	vim.keymap.set("n", "<leader>gs", "<cmd>Telescope lsp_document_symbols<cr>", opts)
+	vim.keymap.set("n", "<leader>gS", "<cmd>Telescope lsp_workspace_symbols<cr>", opts)
+	vim.keymap.set("n", "<leader>gd", "<cmd>Telescope lsp_definitions<cr>", opts)
+	vim.keymap.set("n", "<leader>gt", "<cmd>Telescope lsp_type_definitions<cr>", opts)
 end)
 
 -- (Optional) Configure lua language server for neovim
--- dot_config/nvim/lua/vorslataw/remap.lua
+++ dot_config/nvim/lua/vorslataw/remap.lua
@@ -1,3 +1,4 @@
+-- Leader is space
 vim.g.mapleader = " "
 
 -- Buffer navigation
@@ -6,16 +7,8 @@ vim.keymap.set("n", "<C-j>", "<C-W>j", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-k>", "<C-W>k", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-l>", "<C-W>l", { noremap = true, silent = true })
 
--- Code Actions
-vim.keymap.set("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", { noremap = true, silent = true })
-
--- Smart Rename
-vim.keymap.set("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", { noremap = true, silent = true })
-
--- References
-vim.keymap.set("n", "<leader>gr", "<cmd>lua vim.lsp.buf.references()<CR>", { noremap = true, silent = true })
-
 -- File Viewer
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- Undotree
 vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
