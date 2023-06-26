abe9d61e7c4d04a98adc8fccd1c60f0a62eb6c9c
Sax Dhiut <mail@iyxeyl.me>
2023-06-27 02:16:03 +0400

    I am sorry

--- dot_config/nvim/lua/vorslataw/stuff.lua
+++ dot_config/nvim/lua/vorslataw/stuff.lua
@@ -1,29 +1,39 @@
+-- Appearance
 vim.opt.guicursor = ""
+vim.opt.termguicolors = true
 
+-- Line numbers and relative line numbers
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
+-- Tab settings
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
+-- Indentation
 vim.opt.smartindent = true
 
+-- Wrapping
 vim.opt.wrap = false
 
+-- File handling
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undofile = true
 vim.opt.undodir = os.getenv("HOME") .. "/.config/nvim/undodir"
 
+-- Search
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
-vim.opt.termguicolors = true
-
+-- Scrolling and sign column
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
+
+-- Filename characters
 vim.opt.isfname:append("@-@")
 
+-- CursorHold autocmd event
 vim.opt.updatetime = 50
